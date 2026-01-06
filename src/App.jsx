7d5e14c0fffbfca413c1e62ec6d3845309eb5500
Gokul-RTS <gokul.rtsys@gmail.com>
2026-01-06 17:08:09 +0530

    added a latlng

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScript, Data } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Data, Marker } from "@react-google-maps/api";
 import { useRef } from "react";
 import geoData from "./geoData";
 
@@ -7,12 +7,19 @@ const containerStyle = {
   height: "100vh",
 };
 
+const hyderabadPos = {
+  lat: 17.407645370415413,
+  lng: 78.45112399424097,
+};
+
 function App() {
   const mapRef = useRef(null);
-  const circleRefs = useRef([]);
+  const revenuePolygons = useRef([]);
 
   const onLoadMap = (map) => {
     mapRef.current = map;
+    map.panTo(hyderabadPos);
+    map.setZoom(14);
   };
 
   const onLoadRevenue = (dataLayer) => {
@@ -25,49 +32,33 @@ function App() {
       strokeWeight: 1.2,
     });
 
-    const bounds = new google.maps.LatLngBounds();
-    dataLayer.forEach((f) =>
-      f.getGeometry().forEachLatLng((ll) => bounds.extend(ll))
-    );
-    mapRef.current.fitBounds(bounds);
-  };
-
-  const onLoadPoints = (dataLayer) => {
-    dataLayer.addGeoJson(geoData.circle);
-
     dataLayer.forEach((feature) => {
       const geom = feature.getGeometry();
-      if (geom.getType() !== "Point") return;
 
-      const pos = geom.get();
+      if (geom.getType() === "Polygon") {
+        const paths = [];
+        geom.getArray().forEach((ring) => {
+          paths.push(ring.getArray());
+        });
 
-      const circle = new google.maps.Circle({
-        map: mapRef.current,
-        center: pos,
-        radius: 25, // meters
-        fillColor: "#FF0000",
-        fillOpacity: 0.35,
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 1,
-        clickable: false,
-      });
-
-      circleRefs.current.push(circle);
+        revenuePolygons.current.push(new google.maps.Polygon({ paths }));
+      }
     });
-
-    dataLayer.setMap(null);
   };
 
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+    <LoadScript
+      googleMapsApiKey="AIzaSyCsv8wCUCRDDwdsF1XLV82Yt8m0KqJnK-Y"
+      libraries={["geometry"]}
+    >
       <GoogleMap
         mapContainerStyle={containerStyle}
         onLoad={onLoadMap}
         mapTypeId="satellite"
       >
         <Data onLoad={onLoadRevenue} />
-        <Data onLoad={onLoadPoints} />
+
+        <Marker position={hyderabadPos} title="Hyderabad Point" />
       </GoogleMap>
     </LoadScript>
   );
